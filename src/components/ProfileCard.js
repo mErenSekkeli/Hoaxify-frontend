b61e227d9f67aafcec7caee0561edab2afc91376
mErenSekkeli <erensekkeli46@gmail.com>
2023-07-18 16:01:19 +0300

    new features for profile

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSelector } from "react-redux";
 import {withRouter} from "react-router-dom";
+import defaultPic from "../user_icon.png";
+import ProfileImage from "./ProfileImage.js";
+import { t } from "i18next";
 
 const ProfileCard = (props) => {
     const pathUsername = props.match.params.username;
     const {username} = useSelector((store) => ({username: store.userName}));
+    const {user} = props;
+    const {image, name, surname} = user;
+    const imageSource = image ? `/images/profile/${image}` : defaultPic;
+
+    const [editMode, setEditMode] = useState(false);
+
     let message;
     if(pathUsername === username){
         message = "You are viewing your own profile";
     }else{
         message = "You are viewing " + pathUsername + "'s profile";
     }
-    
+    console.log(message);
     return (
-        <div>
-            {message}
+        <div className="card m-3">
+            <div className="card-header text-center">
+                <ProfileImage user={user} width="200" height="200" hasShadow={true} />
+                <h4>{pathUsername}</h4>
+                <a class="btn btn-outline" title={t('Edit')} onClick={() => setEditMode(true)}><i class="material-symbols-outlined">edit</i></a>
+            </div>
+            <div className="card-body text-center">
+                {!editMode && <span className="font-weight-bold">{name} {surname}</span>}
+            </div>
         </div>
     );
 }
--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -1,11 +1,12 @@
 import React from "react";
 import defaultPic from "../user_icon.png";
 import { Link } from "react-router-dom";
+import ProfileImage from "./ProfileImage.js";
 const UserListItem = (props) => {
     const {user} = props;
     return(
         <Link to={`/user/${user.userName}`} className="list-group-item list-group-item-action">
-            <img className="rounded-circle" alt={user.userName + " Picture"} width="32" height="32" src={(user.image != null) ? user.image : defaultPic } />  
+            <ProfileImage user={user} width="32" height="32" />  
             {user.userName}
         </Link>
       );
