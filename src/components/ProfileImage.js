c4ac1ab1e4642113b816a187efd0101999cf8f30
mErenSekkeli <erensekkeli46@gmail.com>
2023-07-27 11:46:26 +0300

    user profile image progresses added

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -2,11 +2,18 @@ import React from "react";
 import defaultPic from "../user_icon.png";
 
 const ProfileImage = (props) => {
-    const {user, width, height, hasShadow} = props;
+    const {user, width, height, hasShadow, tempimage} = props;
     const {image, username} = user;
-    let imageSource = image ? image : defaultPic;
+    let imageSource = image ? 'images/' + image : defaultPic;
     return(
-        <img className={hasShadow ? "rounded-circle m-1 shadow": "rounded-circle m-1"} width={width ? width : '32'} height={height ? height : '32'} alt={username + " Picture"} src={imageSource}  />
+        <img className={hasShadow ? "rounded-circle m-1 shadow": "rounded-circle m-1"}
+         width={width ? width : '32'} 
+         height={height ? height : '32'} 
+         alt={username + " Picture"} 
+         src={tempimage || imageSource} 
+         onError={(event) => {
+            event.target.src = defaultPic;
+         }} />
     );
 }
 
